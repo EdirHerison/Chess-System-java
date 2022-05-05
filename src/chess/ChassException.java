d25aead5ec01406eacd1bcabe732b81500ab2969
EdirHerison <edir_h@hotmail.com>
2022-05-05 11:51:14 -0300

    Little improvement in board printing

--- src/chess/ChassException.java
+++ src/chess/ChassException.java
@@ -1,7 +1,8 @@
 package chess;
 
 public class ChassException extends RuntimeException {
-	
+	private static final long serialVersionUID = 1L;
+
 	public ChassException (String msg) {
 	   super (msg);	
 	}
